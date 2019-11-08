de906c04297c33f059c6f5ffd95527e8452d1e6a
Andrzej Lamecki <andrzej4172.lama@gmail.com>
2019-11-08 19:50:29 +0100

    Add Variables hierarchy

---
+++ HORNS/Variables/IGenericVariableObserver.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HORNS
+{
+    public interface IVariableObserver<T>
+    {
+        void ValueChanged(T value);
+    }
+}
---
+++ HORNS/Variables/Variable.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HORNS
+{
+    public class Variable
+    {
+        private ICollection<IVariableObserver> observers = new HashSet<IVariableObserver>();
+
+        public void Observe(IVariableObserver observer)
+        {
+            observers.Add(observer);
+        }
+
+        public void Unobserve(IVariableObserver observer)
+        {
+            observers.Remove(observer);
+        }
+
+        private protected void Notify()
+        {
+            foreach(IVariableObserver observer in observers)
+            {
+                observer.ValueChanged();
+            }
+        }
+
+        private protected Variable()
+        {
+
+        }
+    }
+}
