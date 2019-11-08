01f9f7e817231306cef60bde789431c1b3febcf4
Andrzej Lamecki <andrzej4172.lama@gmail.com>
2019-11-08 19:50:29 +0100

    Add Variables hierarchy

---
+++ HORNS/Variables/GenericVariable.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HORNS
+{
+    public class Variable<T> : Variable
+    {
+        private ICollection<IVariableObserver<T>> observers = new HashSet<IVariableObserver<T>>();
+
+        private T _value;
+        private protected virtual T _Value
+        {
+            get
+            {
+                return _value;
+            }
+            set
+            {
+                _value = value;
+                Notify(value);
+            }
+        }
+
+        public T Value
+        {
+            get => _Value;
+            set => _Value = value;
+        }
+
+        private void Notify(T value)
+        {
+            foreach (IVariableObserver<T> observer in observers)
+            {
+                observer.ValueChanged(value);
+            }
+            base.Notify();
+        }
+
+        public void Observe(IVariableObserver<T> observer)
+        {
+            observers.Add(observer);
+        }
+
+        public void Unobserve(IVariableObserver<T> observer)
+        {
+            observers.Remove(observer);
+        }
+
+        public virtual double Evaluate(T value)
+        {
+            return 0;
+        }
+    }
+}
---
+++ HORNS/Variables/Need.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HORNS
+{
+    public abstract class Need<T> : Variable<T>
+    {
+        private protected override T _Value { get => variable.Value; set => variable.Value = value; }
+        private Variable<T> variable;
+        public T Desired { get; private set; }
+
+        public Need(T desired)
+        {
+            Desired = desired;
+        }
+
+        public abstract override double Evaluate(T value);
+    }
+}
