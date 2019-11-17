17b5a7a5476df15ec999e58296cb85c5cb16c6e5
Andrzej Lamecki <andrzej4172.lama@gmail.com>
2019-11-17 11:10:46 +0100

    Implementation of components for boolean variables

---
+++ HORNS/Implementations/Boolean/BooleanPrecondition.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HORNS
+{
+    public class BooleanPrecondition : Precondition<bool, BooleanSolver>
+    {
+        public BooleanPrecondition(Variable<bool> variable, bool value, BooleanSolver solver) : base(variable, solver)
+        {
+            Value = value;
+        }
+
+        public bool Value { get; }
+    }
+}
---
+++ HORNS/Implementations/Boolean/BooleanSolver.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HORNS
+{
+    public class BooleanSolver : VariableSolver<bool, BooleanResult, BooleanPrecondition>
+    {
+        List<BooleanResult> trueResults = new List<BooleanResult>();
+        List<BooleanResult> falseResults = new List<BooleanResult>();
+        protected override IEnumerable<Action> GetActionsSatisfying(BooleanPrecondition precondition)
+        {
+            return GetActions(precondition.Value);
+        }
+
+        protected internal override IEnumerable<Action> GetActionsTowards(Variable<bool> variable, bool desiredValue)
+        {
+            return GetActions(desiredValue);
+        }
+
+        private IEnumerable<Action> GetActions(bool value)
+        {
+            return (value ? trueResults : falseResults).Select(res => res.Action);
+        }
+
+        protected internal override void Register(BooleanResult result)
+        {
+            var destList = result.EndValue ? trueResults : falseResults;
+            destList.Add(result);
+        }
+    }
+}
