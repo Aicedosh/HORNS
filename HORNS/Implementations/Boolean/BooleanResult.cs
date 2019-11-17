bfaa10e3b5ed97a7e14256c75a104095f3ff2915
Andrzej Lamecki <andrzej4172.lama@gmail.com>
2019-11-17 11:10:46 +0100

    Implementation of components for boolean variables

---
+++ HORNS/Implementations/Boolean/BooleanResult.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HORNS
+{
+    public class BooleanResult : ActionResult<bool, BooleanSolver>
+    {
+        public BooleanResult(Variable<bool> variable, bool endValue) : base(variable)
+        {
+            EndValue = endValue;
+        }
+
+        public bool EndValue { get; }
+
+        protected internal override bool GetResultValue(Variable<bool> variable)
+        {
+            return EndValue;
+        }
+    }
+}
