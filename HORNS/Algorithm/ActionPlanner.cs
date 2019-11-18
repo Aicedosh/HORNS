2c4ba203aada0c2ded8e6380cdb6e987184b306f
vrooo <mizuram@student.mini.pw.edu.pl>
2019-11-18 16:59:07 +0100

    Change algorithm concept

--- HORNS/Algorithm/ActionPlanner.cs
+++ HORNS/Algorithm/ActionPlanner.cs
@@ -10,49 +10,16 @@ namespace HORNS
         {
             public float Distance { get; set; }
             // TODO: is Actions actually needed? we're looking at actions per requirement anyway
-            public List<Action> Actions { get; set; } = new List<Action>();
-            public List<RequirementSet> Requirements { get; set; } = new List<RequirementSet>();
+            // public List<Action> Actions { get; set; } = new List<Action>();
+            // TODO: [A] structure to store requirements - something like VariableSet?
+            public HashSet<Requirement> Requirements { get; set; } = new HashSet<Requirement>();
             public ActionPlannerNode Prev { get; set; }
             public Action PrevAction { get; set; }
-            public VariableSet VariablePatch { get; set; }
+            // public VariableSet VariablePatch { get; set; }
 
             public ActionPlannerNode(float dist = float.MaxValue)
             {
                 Distance = dist;
-            }
-        }
-
-        private class RequirementSet
-        {
-            public Action Action { get; set; }
-            public List<Requirement> Requirements { get; set; } = new List<Requirement>();
-            public bool Closed { get; private set; } = false;
-
-            public RequirementSet(Action action, VariableSet variables)
-            {
-                Action = action;
-                foreach (var pre in action.GetPreconditions())
-                {
-                    Requirements.Add(pre.GetRequirement(variables));
-                }
-            }
-
-            public RequirementSet(RequirementSet set)
-            {
-                Action = set.Action;
-                foreach (var req in set.Requirements)
-                {
-                    Requirements.Add(req.Clone());
-                }
-            }
-
-            public bool IsClosed()
-            {
-                //Closed = false;
-                //foreach (var req in Requirements)
-                //{
-                    
-                //}
             }
         }
         
@@ -71,7 +38,7 @@ namespace HORNS
                 needs.Add((need, need.GetPriority()));
             }
             // lower "priority" => more important need
-            needs.Sort((x, y) => x.Priority.CompareTo(y.Priority));     // TODO: this can be done faster
+            needs.Sort((x, y) => x.Priority.CompareTo(y.Priority));     // TODO: optimize
             if (needs.Count > possibleGoals)
             {
                 needs.RemoveRange(possibleGoals, needs.Count - possibleGoals);
@@ -83,66 +50,80 @@ namespace HORNS
             foreach (var (need, priority) in needs)
             {
                 var open = new SimplePriorityQueue<ActionPlannerNode>();
-                // TODO: are we using close?
+                // TODO: what to do about close? compare fulfilled/unfulfilled requirements?
                 //var close = new HashSet<ActionPlannerNode>();
 
                 var goal = new ActionPlannerNode(0);
-                goal.VariablePatch = new VariableSet();
-                open.Enqueue(goal, goal.Distance);      // TODO: something smarter than adding the same value twice
+                foreach (var action in need.GetActionsTowards())
+                {
+                    var nodeFromGoal = new ActionPlannerNode(action.CachedCost);
+                    nodeFromGoal.Prev = goal;
+                    nodeFromGoal.PrevAction = action;
+                    open.Enqueue(nodeFromGoal, nodeFromGoal.Distance);  // TODO: something smarter than adding the same value twice
+                }
 
                 ActionPlannerNode last = null;
                 while(open.Count > 0)
                 {
                     var node = open.Dequeue();
-
                     // TODO: add to close?
-                    bool reqLeft = false;
-                    if (node.Prev != null)
+
+                    if (node.Prev != null)  // TODO: unnecessary check if we're moving goal's iteration outside
                     {
-                        node.VariablePatch = node.Prev.VariablePatch.Clone();
-                        foreach (var reqSet in node.Prev.Requirements)
+                        bool reqLeft = false;
+                        foreach (var req in node.Prev.Requirements)
                         {
-                            node.Requirements.Add(new RequirementSet(reqSet));
+                            node.Requirements.Add(req.Clone());
                         }
-                        node.Requirements.Add(new RequirementSet(node.PrevAction, node.VariablePatch));
-
-                        foreach (var reqSet in node.Requirements)
+                        foreach (var pre in node.PrevAction.GetPreconditions())
                         {
+                            node.Requirements.Add(pre.GetRequirement());
+                        }
 
+                        // TODO: [A] please make sure this can be done!
+                        node.PrevAction.SubtractResults(node.Requirements);
+                        foreach (var req in node.Requirements)
+                        {
+                            // TODO: move fulfilled to another set? after we figure out what to do abt partial fulfillment
+                            if (!req.Fulfilled && !req.IsFulfilled(agent.Variables))
+                            {
+                                reqLeft = true;
+                                break;
+                            }
+                        }
+
+                        if (!reqLeft)
+                        {
+                            last = node;
+                            break;
                         }
                     }
-
-                    // TODO: also consider actions for need
-                    foreach (var reqItem in node.Requirements)
+                    
+                    foreach (var req in node.Requirements)
                     {
-                        for (int i = 0; i < reqItem.Requirements.Count; i++)
+                        var actions = req.GetActions();
+                        foreach (var action in actions)
                         {
-                            var req = reqItem.Requirements[i];
-                            var actions = req.GetActions();
-                            foreach (var action in actions)
-                            {
-                                // from Dumbledore:
-                                //if (w in OPEN)
-                                //{
-                                //    odległość[w] = nieskończoność
-                                //    wstaw w do OPEN
-                                //}
-                                //if (odległość[w] > odległość[u] + waga<u, w>)
-                                //{
-                                //    odległość[w] = odległość[u] + waga < u,w >
-                                //    aktualizacja priorytetu wierzchołka w(w OPEN)
-                                //    poprzedni[w] = u
-                                //}
+                            // from Dumbledore:
+                            //if (w in OPEN)
+                            //{
+                            //    odległość[w] = nieskończoność
+                            //    wstaw w do OPEN
+                            //}
+                            //if (odległość[w] > odległość[u] + waga<u, w>)
+                            //{
+                            //    odległość[w] = odległość[u] + waga < u,w >
+                            //    aktualizacja priorytetu wierzchołka w(w OPEN)
+                            //    poprzedni[w] = u
+                            //}
 
-                                // TODO: check if already exists?
-                                var newNode = new ActionPlannerNode(node.Distance + action.CachedCost);
-                                // copy as little as possible on this stage; copy the rest when we visit it
-                                newNode.Prev = node;
-                                newNode.PrevAction = action;
+                            // TODO: check if already exists?
+                            var newNode = new ActionPlannerNode(node.Distance + action.CachedCost);
+                            // copy as little as possible at this stage; copy the rest when we visit it
+                            newNode.Prev = node;
+                            newNode.PrevAction = action;
 
-                                open.Enqueue(newNode, newNode.Distance);
-
-                            }
+                            open.Enqueue(newNode, newNode.Distance);
                         }
                     }
                 }
--- HORNS/Preconditions/Requirement.cs
+++ HORNS/Preconditions/Requirement.cs
@@ -8,8 +8,9 @@ namespace HORNS
     {
         protected internal abstract bool IsEqual(Requirement other);
         protected internal abstract IEnumerable<Action> GetActions();
-        internal bool Fulfilled { get; private set; } = false;
-        internal abstract bool IsFulfilled(VariableSet variablePatch);
+        internal bool Fulfilled { get; private protected set; } = false;    // TODO: property + methods = dis ugly
+        internal abstract bool IsFulfilled();
+        internal abstract bool IsFulfilled(VariableSet variables);
         internal abstract Requirement Clone();
     }
 }
