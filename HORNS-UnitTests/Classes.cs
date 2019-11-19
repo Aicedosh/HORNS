3db14cbda1a0104a70b88e261f9a7a8b890dce86
vrooo <mizuram@student.mini.pw.edu.pl>
2019-11-20 00:35:24 +0100

    Add basic unit tests

---
+++ HORNS-UnitTests/Classes.cs
@@ -0,0 +1,45 @@
+﻿using HORNS;
+
+namespace HORNS_UnitTests
+{
+    // ACTIONS
+
+    // BasicAction
+    class BasicAction : Action
+    {
+        protected override void ActionResult()
+        {
+        }
+    }
+
+    // ChangeStateAction
+    class State
+    {
+        public bool Value { get; set; } = false;
+    }
+
+    class ChangeStateAction : Action
+    {
+        public State State { get; private set; } = new State();
+        protected override void ActionResult()
+        {
+            State.Value = true;
+        }
+    }
+
+    // NEEDS
+
+    // BooleanNeed
+    class BooleanNeed : Need<bool>
+    {
+        public BooleanNeed(Variable<bool> variable, bool desired, VariableSolver<bool> solver)
+            : base(variable, desired, solver)
+        {
+        }
+
+        public override float Evaluate(bool value)
+        {
+            return value ? 100 : 1;
+        }
+    }
+}
