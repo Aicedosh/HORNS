e823c5b44bd89ddb00191e6b02d0a4cab4d49ed6
vrooo <mizuram@student.mini.pw.edu.pl>
2019-11-19 12:53:56 +0100

    Add integer support along with an example

---
+++ HORNS/Implementations/Integer/IntegerAddResult.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HORNS
+{
+    public class IntegerAddResult : ActionResult<int, IntegerSolver>
+    {
+        public int Term { get; }
+
+        public IntegerAddResult(Variable<int> variable, int term) : base(variable)
+        {
+            Term = term;
+        }
+
+        protected internal override int GetResultValue(Variable<int> variable)
+        {
+            return variable.Value + Term;
+        }
+
+        // TODO: is this necessary to override? maybe it's enough to Apply always using GetResultValue?
+        internal override void Apply(IdSet<Variable> variables)
+        {
+            if (!variables.Contains(Variable.Id))
+            {
+                variables.Add(Variable.GetCopy());
+            }
+            var variable = variables[Variable.Id] as Variable<int>;
+            variable.Value = GetResultValue(variable);
+        }
+    }
+}
---
+++ HORNS/Implementations/Integer/IntegerSolver.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HORNS
+{
+    public class IntegerSolver : VariableSolver<int, IntegerAddResult, IntegerPrecondition>
+    {
+        List<IntegerAddResult> positiveResults = new List<IntegerAddResult>();
+        List<IntegerAddResult> negativeResults = new List<IntegerAddResult>();
+
+        protected override IEnumerable<Action> GetActionsSatisfying(IntegerPrecondition precondition)
+        {
+            return (precondition.Direction == IntegerPrecondition.Condition.AtLeast ? positiveResults : negativeResults)
+                .Select(res => res.Action);
+        }
+
+        protected internal override IEnumerable<Action> GetActionsTowards(Variable<int> variable, int desiredValue)
+        {
+            if (variable.Value == desiredValue)
+            {
+                return new List<Action>();
+            }
+            return (variable.Value < desiredValue ? positiveResults : negativeResults).Select(res => res.Action);
+        }
+
+        protected internal override void Register(IntegerAddResult result)
+        {
+            if (result.Term > 0)
+            {
+                positiveResults.Add(result);
+            }
+            else if (result.Term < 0)
+            {
+                negativeResults.Add(result);
+            }
+        }
+    }
+}
