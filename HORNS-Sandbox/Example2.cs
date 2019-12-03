f97f1695bbc099d039ff75b3c14be99db0dcc4b2
Andrzej Lamecki <andrzej4172.lama@gmail.com>
2019-12-03 14:59:28 +0100

    Fix need evaluation functions

--- HORNS-Sandbox/Example2.cs
+++ HORNS-Sandbox/Example2.cs
@@ -27,12 +27,15 @@ namespace HORNS_Sandbox
 
         private class Hunger : Need<int>
         {
-            public Hunger(Variable<int> variable, int desired) : base(variable, desired, v => 100 - v) { }
+            public Hunger(Variable<int> variable, int desired) : base(variable, desired, v =>
+            v > 100 ? -100 :
+            (float)(50*Math.Log10(-v+1+100))) { }
         }
 
         private class Energy : Need<int>
         {
-            public Energy(Variable<int> variable, int desired) : base(variable, desired, v => (float)Math.Log10(v + 1)) { }
+            public Energy(Variable<int> variable, int desired) : base(variable, desired, v => 
+            v < 0 ? 100 : (float)(50*Math.Log10(v + 1))) { }
 
             protected override bool IsSatisfied(int value)
             {
@@ -43,8 +46,8 @@ namespace HORNS_Sandbox
         public static void Run()
         {
             var hasAxe    = new BoolVariable();
-            var hunger    = new IntVariable(50);
-            var energy    = new IntVariable(105);
+            var hunger    = new IntVariable(100);
+            var energy    = new IntVariable(100);
             var money     = new IntVariable();
             var wood      = new IntVariable();
             var chairs    = new IntVariable();
@@ -54,65 +57,64 @@ namespace HORNS_Sandbox
             var feelingSoupy = new BoolVariable();
 
             var pickAxe = new MessageAction("Picked up an axe");
-            pickAxe.AddCost(10);
+            //pickAxe.AddCost(10);
             pickAxe.AddPrecondition(hasAxe, new BooleanPrecondition(false));
             pickAxe.AddResult(hasAxe, new BooleanResult(true));
 
             var chopTree = new MessageAction("Chopped down a tree");
-            chopTree.AddCost(50);
+            //chopTree.AddCost(50);
             chopTree.AddPrecondition(hasAxe, new BooleanPrecondition(true));
             chopTree.AddResult(wood, new IntegerAddResult(1));
-            chopTree.AddResult(energy, new IntegerAddResult(-10));
+            chopTree.AddResult(energy, new IntegerAddResult(-2));
 
             var sellWood = new MessageAction("Sold a piece of wood");
-            sellWood.AddCost(30);
+            //sellWood.AddCost(30);
             sellWood.AddPrecondition(wood, new IntegerPrecondition(1, IntegerPrecondition.Condition.AtLeast));
             sellWood.AddResult(wood, new IntegerAddResult(-1));
             sellWood.AddResult(money, new IntegerAddResult(1));
 
             var makeChair = new MessageAction("Made a chair");
-            makeChair.AddCost(50);
+            //makeChair.AddCost(5);
             makeChair.AddPrecondition(wood, new IntegerPrecondition(1, IntegerPrecondition.Condition.AtLeast));
             makeChair.AddResult(wood, new IntegerAddResult(-1));
             makeChair.AddResult(chairs, new IntegerAddResult(1));
-            makeChair.AddResult(energy, new IntegerAddResult(-5));
+            makeChair.AddResult(energy, new IntegerAddResult(-3));
 
             var sellChair = new MessageAction("Sold a chair");
-            sellChair.AddCost(30);
+            //sellChair.AddCost(30);
             sellChair.AddPrecondition(chairs, new IntegerPrecondition(1, IntegerPrecondition.Condition.AtLeast));
             sellChair.AddResult(chairs, new IntegerAddResult(-1));
             sellChair.AddResult(money, new IntegerAddResult(3));
 
             var buyRzodkiew = new MessageAction("Bought a rzodkiew");
-            buyRzodkiew.AddCost(30);
+            //buyRzodkiew.AddCost(30);
             buyRzodkiew.AddPrecondition(money, new IntegerPrecondition(3, IntegerPrecondition.Condition.AtLeast));
             buyRzodkiew.AddResult(money, new IntegerAddResult(-3));
             buyRzodkiew.AddResult(rzodkiews, new IntegerAddResult(1));
 
-            var eatRzodkiew = new MessageAction("Ate a rzodkiew");
-            eatRzodkiew.AddCost(10);
-            eatRzodkiew.AddCost(feelingSoupy, x => x ? 1000 : 0);
+            var eatRzodkiew = new MessageAction("Ate a rzodkiew++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+            //eatRzodkiew.AddCost(20);
             eatRzodkiew.AddPrecondition(rzodkiews, new IntegerPrecondition(1, IntegerPrecondition.Condition.AtLeast));
             eatRzodkiew.AddResult(rzodkiews, new IntegerAddResult(-1));
-            eatRzodkiew.AddResult(hunger, new IntegerAddResult(-20));
+            eatRzodkiew.AddResult(hunger, new IntegerAddResult(-5));
 
-            var makeSoup = new MessageAction("Made some soup");
-            makeSoup.AddCost(20);
+            var makeSoup = new MessageAction("Made some soup__________________________________________________________________________");
+            //makeSoup.AddCost(5);
             makeSoup.AddPrecondition(rzodkiews, new IntegerPrecondition(2, IntegerPrecondition.Condition.AtLeast));
             makeSoup.AddResult(rzodkiews, new IntegerAddResult(-2));
             makeSoup.AddResult(soups, new IntegerAddResult(1));
-            makeSoup.AddResult(energy, new IntegerAddResult(-3));
+            makeSoup.AddResult(energy, new IntegerAddResult(-11));
 
             var eatSoup = new MessageAction("Ate some soup");
-            eatSoup.AddCost(1);
+            //eatSoup.AddCost(1);
             eatSoup.AddPrecondition(soups, new IntegerPrecondition(1, IntegerPrecondition.Condition.AtLeast));
             eatSoup.AddResult(soups, new IntegerAddResult(-1));
-            eatSoup.AddResult(hunger, new IntegerAddResult(-60));
+            eatSoup.AddResult(hunger, new IntegerAddResult(-20));
             eatSoup.AddResult(energy, new IntegerAddResult(1));
 
-            var sleep = new MessageAction("Went to sleep");
+            var sleep = new MessageAction("Went to sleep$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
             sleep.AddCost(10);
-            sleep.AddResult(energy, new IntegerAddResult(100));
+            sleep.AddResult(energy, new IntegerAddResult(25));
 
             var idle = new MessageAction("Is bored");
             idle.AddCost(100000);
@@ -130,9 +132,10 @@ namespace HORNS_Sandbox
             Random random = new Random(42);
             while (true)
             {
-                energy.Value -= 1;
-                hunger.Value += 5;
-                feelingSoupy.Value = random.Next(2) == 0;
+                //energy.Value -= 1;
+                hunger.Value += random.Next(3);
+
+                Console.WriteLine($"    Energy: {energy.Value} Hunger: {hunger.Value}");
 
                 var nextAction = agent.GetNextAction();
                 if (nextAction == null)
@@ -144,7 +147,7 @@ namespace HORNS_Sandbox
                     nextAction.Perform();
                 }
 
-                Thread.Sleep(500);
+                Console.ReadLine();
             }
         }
     }
